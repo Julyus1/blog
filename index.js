66b566f81550bd8b7ef52984363955232fed5484
julyus1 <jmorin0100@gmail.com>
2026-02-17 19:04:15 +0800

    added post submission localy no db yet

--- index.js
+++ index.js
@@ -5,9 +5,26 @@ import bodyParser from "body-parser"
 const app = express()
 const port = 3000
 
+let data = []
+let post = {}
+
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static("public"))
 app.get("/", (req, res) => {
-    res.render("index.ejs")
+
+    res.render("index.ejs", { posts: data })
+})
+app.get("/create", (req, res) => {
+    res.render("post.ejs")
+})
+app.post("/submit", (req, res) => {
+
+    post = { title: req.body["title-post"], blogPost: req.body["post-blog"] }
+
+    data.push(post)
+
+    res.render("post.ejs")
 })
 
 app.listen(port, () => {
